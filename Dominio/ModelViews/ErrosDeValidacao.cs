0d4480f62b0761d5a1ca4742f95deadb6b49743e
GabrielSilvaVG <gabrieldepaulajf@gmail.com>
2025-11-04 23:03:27 -0300

    Criando validação ao cadastrar e atualizar um veiculo

---
+++ Dominio/ModelViews/ErrosDeValidacao.cs
@@ -0,0 +1,7 @@
+namespace minimal.Dominio.ModelViews
+{
+    public struct ErrosDeValidacao
+    {
+        public List<string> Mensagens { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -42,13 +42,32 @@ app.MapPost("/Administradores/login", ([FromBody] LoginDTO loginDto, IAdministra
 #endregion
 
 #region Veiculos
-app.MapPost("/Veiculos", ([FromBody] VeiculoDTO veiculoDto, IVeiculoServico veiculoServico) =>
+static ErrosDeValidacao ValidaDTO(VeiculoDTO veiculoDto)
 {
+    var validacao = new ErrosDeValidacao
+    {
+        Mensagens = []
+    };
+    if (string.IsNullOrEmpty(veiculoDto.Nome))
+        validacao.Mensagens.Add("O nome do veículo não pode ser vazio.");
+    if (string.IsNullOrEmpty(veiculoDto.Marca))
+        validacao.Mensagens.Add("A marca do veículo não pode ser vazia.");
+    if (veiculoDto.Ano < 1886 || veiculoDto.Ano > DateTime.Now.Year + 1)
+        validacao.Mensagens.Add("O ano do veículo é inválido.");
+    return validacao;
+}
+
+app.MapPost("/Veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+    var validacao = ValidaDTO(veiculoDTO);
+    if (validacao.Mensagens.Count > 0)
+        return Results.BadRequest(validacao);
+
     var veiculo = new Veiculo
     {
-        Nome = veiculoDto.Nome,
-        Marca = veiculoDto.Marca,
-        Ano = veiculoDto.Ano
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano
     };
     veiculoServico.Incluir(veiculo);
     return Results.Created($"/Veiculos/{veiculo.Id}", veiculo);
@@ -71,9 +90,13 @@ app.MapGet("/Veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico
 
 app.MapPut("/Veiculos/{id}", ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
 {
-
+    
     var veiculo = veiculoServico.BuscaPorId(id);
     if (veiculo == null) return Results.NotFound();
+
+    var validacao = ValidaDTO(veiculoDTO);
+    if (validacao.Mensagens.Count > 0)
+        return Results.BadRequest(validacao);
 
     veiculo.Nome = veiculoDTO.Nome;
     veiculo.Marca = veiculoDTO.Marca;
